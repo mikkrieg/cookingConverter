051f4f1f81e00f8803a1d62ca3638c2ffb1a7ca8
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-13 21:45:45 -0800

    Add business logic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
+function cupsToLiters(cups) {
+  const result = cups / 4.167;
+  return result
+}
+
+function litersToGallons(liters) {
+  const result = liters / 3.785;
+  return result
+}
+
+function gramsToOunces(grams) {
+  const result = grams / 28.35;
+  return result
+}
+
+function ouncesToPounds(ounces) {
+  const result = ounces / 16;
+  return result
+}
+
+
+
+
+
+
+
 $(document).ready(function() {
-  $()
+  $("")
 
 
 
